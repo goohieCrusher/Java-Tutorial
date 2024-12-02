962318ca2f3dd2115775e8c8f7119b7768070a58
Aiwen Bao <aiwenbaol@gmail.com>
2024-12-01 19:26:41 -0800

    fixed it up

---
+++ src/src/tutorial/EasyMath.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package tutorial;
+
+/**
+ * Easy Math 4 Awien.
+ */
+public class EasyMath {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    int number1 = 6;
+    int number2 = 144;
+    
+    int num3 = number1 + number2; 
+    System.out.println("sum is " + num3);
+
+    int num4 = number2 - number1; 
+    int num5 = number1 * number2; 
+    int num6 = number2 / number1; 
+    System.out.println("difference is " + num4);
+    System.out.println("product is " + num5);
+    System.out.println("quotient is " + num6);
+
+  }
+
+}
